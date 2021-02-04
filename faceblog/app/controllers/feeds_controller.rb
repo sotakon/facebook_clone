7ff6823c013890ce791df737cd5f79c6e7cb7fa7
sotakon <skon1221@gmail.com>
2021-02-04 15:33:01 +0900

    最新

--- faceblog/app/controllers/feeds_controller.rb
+++ faceblog/app/controllers/feeds_controller.rb
@@ -1,6 +1,7 @@
 class FeedsController < ApplicationController
   protect_from_forgery
   before_action :set_feed, only: %i[show edit update destroy ]
+  before_action :authenticate_user, {only: [:create]}
   def index
     @feeds = Feed.all
   end
@@ -13,13 +14,9 @@ class FeedsController < ApplicationController
       @feed = Feed.new
     end
   end
-  def confirm
-    @feed = Feed.new(feed_params)
-  end
-  def edit
-  end
   def create
     @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
     respond_to do |format|
       if @feed.save
         format.html { redirect_to @feed, notice: "Feed was successfully created." }
@@ -29,6 +26,12 @@ class FeedsController < ApplicationController
         format.json { render json: @feed.errors, status: :unprocessable_entity }
       end
     end
+  end
+  def confirm
+    @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
+  end
+  def edit
   end
   def update
     respond_to do |format|
@@ -56,5 +59,12 @@ class FeedsController < ApplicationController
   def feed_params
     params.require(:feed).permit(:image, :image_cache, :content, :user_id)
     # .merge(user_id: current_user.id)
+  end
+  def authenticate_user
+  # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
+    if @current_user == nil
+    flash[:notice] = t('notice.login_needed')
+    redirect_to new_session_path
+    end
   end
 end
