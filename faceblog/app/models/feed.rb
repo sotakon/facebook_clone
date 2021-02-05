87bdf7667101599be0ca83a692dc0201940f7134
sotakon <skon1221@gmail.com>
2021-02-05 23:07:48 +0900

    バリテーション、確認画面変更

--- faceblog/app/models/feed.rb
+++ faceblog/app/models/feed.rb
@@ -2,4 +2,5 @@ class Feed < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user, optional: true
   validates :content, presence: true
+  validates :image, presence: true
 end
